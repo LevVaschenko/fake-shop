9d316388a39cf1d6aac043231ffbb292a7d00594
imac <levvaschenko@gmail.com>
2022-10-30 22:43:41 -0700

    add AppBar and Container lg

--- src/container/Header/Header.tsx
+++ src/container/Header/Header.tsx
@@ -1,11 +1,42 @@
 import React from 'react'
+import AppBar from '@mui/material/AppBar'
+import Toolbar from '@mui/material/Toolbar'
+import Typography from '@mui/material/Typography'
+import Button from '@mui/material/Button'
+import IconButton from '@mui/material/IconButton'
+import MenuIcon from '@mui/icons-material/Menu'
+import Container from '@mui/material/Container'
 
 type Props = {}
 
 const Header = (props: Props) => {
-  return (
-    <div>Header</div>
-  )
+    return (
+        <>
+            <AppBar position="static">
+                <Container maxWidth="lg">
+                    <Toolbar>
+                        <IconButton
+                            size="large"
+                            edge="start"
+                            color="inherit"
+                            aria-label="menu"
+                            sx={{ mr: 2 }}
+                        >
+                            <MenuIcon />
+                        </IconButton>
+                        <Typography
+                            variant="h6"
+                            component="div"
+                            sx={{ flexGrow: 1 }}
+                        >
+                            Fake Shop
+                        </Typography>
+                        <Button color="inherit">Login</Button>
+                    </Toolbar>
+                </Container>
+            </AppBar>
+        </>
+    )
 }
 
 export default Header
