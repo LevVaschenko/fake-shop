4ce35e7d0abdb2b4b8ba6621a4c87818228aa58d
imac <levvaschenko@gmail.com>
2022-10-30 21:26:21 -0700

    add named export

--- src/App.tsx
+++ src/App.tsx
@@ -1,68 +1,9 @@
 import React from 'react'
 
-// React Element
-// const title = <h1>Hello React.js</h1>
+type Props = {}
 
-const list = (
-    <ul>
-        <li>Item 1</li>
-        <li>Item 2</li>
-        <li>Item 3</li>
-    </ul>
-)
-
-const List = () => {
-    return (
-        <>
-            <ul>
-                <li>123</li>
-                <li>456</li>
-                <li>789</li>
-            </ul>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste
-                distinctio, id ad totam aperiam enim fuga quasi libero nostrum
-                architecto ipsum? A adipisci architecto fugit ab quam? Incidunt,
-                ipsa sapiente!
-            </p>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste
-                distinctio, id ad totam aperiam enim fuga quasi libero nostrum
-                architecto ipsum? A adipisci architecto fugit ab quam? Incidunt,
-                ipsa sapiente!
-            </p>
-        </>
-    )
+export const App = (props: Props) => {
+    return <div>App</div>
 }
 
-// React Component
-const Title = () => <h1>Hello React Component</h1>
-
-type TitleProps = {
-    title: string
-    text: string
-    num: number
-}
-const TitleApp = (props: TitleProps) => {
-    return (
-        <>
-            <h1>
-                {props.text} {props.title}
-            </h1>
-            {props.num}
-        </>
-    )
-}
-
-function App() {
-    return (
-        <div className="App">
-            <Title />
-            <TitleApp title="App.js" text="Hi" num={10} />
-            <TitleApp title="Test.js" text="Hello" num={25} />
-            <List />
-        </div>
-    )
-}
-
-export default App
+// export default App
