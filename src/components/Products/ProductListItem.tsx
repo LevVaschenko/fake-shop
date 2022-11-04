4778b3d92f43b716ef9909b77bb08f0eb38a84c2
imac <levvaschenko@gmail.com>
2022-11-03 17:52:34 -0700

    Converting a Function to a Class

--- src/components/Products/ProductListItem.tsx
+++ src/components/Products/ProductListItem.tsx
@@ -5,7 +5,7 @@ import {
     CardContent,
     TextField,
 } from '@mui/material'
-import React from 'react'
+import React, { Component } from 'react'
 import './ProductListItem.scss'
 
 type Props = {
@@ -17,46 +17,45 @@ type Props = {
     image: string
 }
 
-const ProductListItem = ({
-    name,
-    description,
-    type,
-    size,
-    price,
-    image,
-}: Props) => {
-    return (
-        <Card className="product">
-            <CardContent>
-                <div className="product-image">
-                    <img src={image} alt="" />
-                </div>
-                <h4>{name}</h4>
-                <p className="product-description">{description}</p>
-                <div className="product-features">
-                    <span>Type: </span>
-                    {type}
-                </div>
-                <div className="product-features">
-                    <span>Size: </span>
-                    {size} GB
-                </div>
-                <div className="product-price">Price: {price} $</div>
-                <div className="product-quantity">
-                    <Button variant="contained" size="medium">
-                        -
-                    </Button>
-                    <TextField size="small" value="1" />
-                    <Button variant="contained" size="medium">
-                        +
-                    </Button>
-                </div>
-            </CardContent>
-            <CardActions className="button-wrap">
-                <Button variant="contained">Add to cart</Button>
-            </CardActions>
-        </Card>
-    )
+class ProductListItem extends Component <Props> {
+    render() {
+        return (
+            <Card className="product">
+                <CardContent>
+                    <div className="product-image">
+                        <img src={this.props.image} alt="" />
+                    </div>
+                    <h4>{this.props.name}</h4>
+                    <p className="product-description">
+                        {this.props.description}
+                    </p>
+                    <div className="product-features">
+                        <span>Type: </span>
+                        {this.props.type}
+                    </div>
+                    <div className="product-features">
+                        <span>Size: </span>
+                        {this.props.size} GB
+                    </div>
+                    <div className="product-price">
+                        Price: {this.props.price} $
+                    </div>
+                    <div className="product-quantity">
+                        <Button variant="contained" size="medium">
+                            -
+                        </Button>
+                        <TextField size="small" value="1" />
+                        <Button variant="contained" size="medium">
+                            +
+                        </Button>
+                    </div>
+                </CardContent>
+                <CardActions className="button-wrap">
+                    <Button variant="contained">Add to cart</Button>
+                </CardActions>
+            </Card>
+        )
+    }
 }
 
 export default ProductListItem
