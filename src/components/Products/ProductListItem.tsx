4b96bb79eaa2339a4ed0ce6aaf90d40edaea10ef
imac <levvaschenko@gmail.com>
2022-11-09 18:45:25 -0800

    add useState logic and disabled state

--- src/components/Products/ProductListItem.tsx
+++ src/components/Products/ProductListItem.tsx
@@ -5,7 +5,7 @@ import {
     CardContent,
     TextField,
 } from '@mui/material'
-import React from 'react'
+import React, { useState } from 'react'
 import './ProductListItem.scss'
 
 type Props = {
@@ -25,6 +25,20 @@ const ProductListItem = ({
     price,
     image,
 }: Props) => {
+    const [count, setCount] = useState<number>(1)
+    const [color, setColor] = useState<string>('Green')
+
+    const onIncrementClick = () =>
+        setCount((prevState: number) => prevState + 1)
+
+    const onDecrementClick = () =>
+        setCount((prevState: number) => prevState - 1)
+
+    const changeColor = () =>
+        setColor((prevState: string) =>
+            prevState === 'Green' ? 'Red' : 'Green'
+        )
+
     return (
         <Card className="product">
             <CardContent>
@@ -42,12 +56,31 @@ const ProductListItem = ({
                     {size} GB
                 </div>
                 <div className="product-price">Price: {price} $</div>
+                <div>
+                    <p className="color">Color: {color}</p>
+                    <button
+                        onClick={changeColor}
+                        className="change-color-button"
+                    >
+                        Change Color
+                    </button>
+                </div>
                 <div className="product-quantity">
-                    <Button variant="contained" size="medium">
+                    <Button
+                        variant="contained"
+                        size="medium"
+                        onClick={onDecrementClick}
+                        disabled={count <= 1}
+                    >
                         -
                     </Button>
-                    <TextField size="small" value="1" />
-                    <Button variant="contained" size="medium">
+                    <TextField size="small" value={count} />
+                    <Button
+                        variant="contained"
+                        size="medium"
+                        onClick={onIncrementClick}
+                        disabled={count >= 5}
+                    >
                         +
                     </Button>
                 </div>
